f550b4675f79c93726872f3a6b4f10b6fcd6c259
inungg <125361551+inungg@users.noreply.github.com>
2023-05-26 00:11:44 +0800

    Update Block.cpp

--- Block.cpp
+++ Block.cpp
@@ -1,66 +1,63 @@
 #include "Block.h"
 
-Block::Block(Cell pos)
-    :pos{ pos } {
-    srand(time(0));
-    type = static_cast<block_type>(rand() % 7);
+Block::Block(Point pos)
+    :pos{pos} {
+        srand(time(0));
+        type=static_cast<BlockType>(rand()%7);
+        if(type==Block_t)
+            body={pos,pos.shiftCopy(1,0),pos.shiftCopy(-1,0),pos.shiftCopy(0,1)};
+        else if(type==Block_i)
+            body={pos,pos.shiftCopy(0,-1),pos.shiftCopy(0,1),pos.shiftCopy(0,2)};
+        else if(type==Block_o)
+            body={pos,pos.shiftCopy(0,1),pos.shiftCopy(1,0),pos.shiftCopy(1,1)};
+        else if(type==Block_l)
+            body={pos,pos.shiftCopy(0,1),pos.shiftCopy(0,-1),pos.shiftCopy(1,-1)};
+        else if(type==Block_j)
+            body={pos,pos.shiftCopy(-1,0),pos.shiftCopy(0,1),pos.shiftCopy(0,2)};
+        else if(type==Block_s)
+            body={pos,pos.shiftCopy(-1,0),pos.shiftCopy(0,1),pos.shiftCopy(1,1)};
+        else if(type==Block_z)
+            body={pos,pos.shiftCopy(1.0),pos.shiftCopy(0,1),pos.shiftCopy(-1,1)};
 
-    if (type == t_block)
-        body = { pos, pos.shift_copy(1, 0), pos.shift_copy(-1, 0), pos.shift_copy(0, 1) };
-    else if (type == i_block)
-        body = { pos, pos.shift_copy(0, -1), pos.shift_copy(0, 1), pos.shift_copy(0, 2) };
-    else if (type == o_block)
-        body = { pos, pos.shift_copy(0, 1), pos.shift_copy(1, 0), pos.shift_copy(1, 1) };
-    else if (type == l_block)
-        body = { pos, pos.shift_copy(0, 1), pos.shift_copy(0, -1), pos.shift_copy(1, -1) };
-    else if (type == j_block)
-        body = { pos, pos.shift_copy(-1, 0), pos.shift_copy(0, 1), pos.shift_copy(0, 2) };
-    else if (type == s_block)
-        body = { pos, pos.shift_copy(-1, 0), pos.shift_copy(0, 1), pos.shift_copy(1, 1) };
-    else if (type == z_block)
-        body = { pos, pos.shift_copy(1, 0), pos.shift_copy(0, 1), pos.shift_copy(-1, 1) };
 }
-
-void Block::fall_down() {
-    pos.move(0, -1);
-    for (auto& point : body)
-        point.move(0, -1);
-}
-
-std::vector<Cell> Block::next_fall_down_body() {
-    std::vector<Cell> next_body;
-    for (const auto& point : body)
-        next_body.push_back(point.shift_copy(0, -1));
-    return next_body;
-}
-
-void Block::move(enum move_direction dir) {
-    pos.move(dir, 0);
-    for (auto& point : body)
-        point.move(dir, 0);
-}
-
-std::vector<Cell> Block::next_move_body(enum move_direction dir) {
-    std::vector<Cell> next_body;
-    for (const auto& point : body)
-        next_body.push_back(point.shift_copy(dir, 0));
-    return next_body;
-}
-
-void Block::rotate() {
-    for (auto& point : body) {
-        int point_x = point.get_x() - pos.get_x();
-        int point_y = point.get_y() - pos.get_y();
-        point.set_coordinate((-1 * point_y) + pos.get_x(), point_x + pos.get_y());
+    void Block::fallDown(){
+        pos.move(0,-1);
+        for(auto &point:body)
+            point.move(0,-1);
     }
-}
+    vector<Point> Block::nextFallDownBody(){
+        vector<Point> nextBody;
+        for(const auto &point:body)
+            nextBody.push_back(point.shiftCopy(0,-1));
 
-std::vector<Cell> Block::next_rotate_body() {
-    std::vector<Cell> next_body;
-    for (const auto& point : body) {
-        int point_x = point.get_x() - pos.get_x();
-        int point_y = point.get_y() - pos.get_y();
-        next_body.push_back(Cell((-1 * point_y) + pos.get_x(), point_x + pos.get_y()));
+        return nextBody;
     }
-    return next_body;
-}
+    void Block::move(enum moveDirection dir){
+        pos.move(dir,0);
+        for(auto &point:body)
+            point.move(dir,0);
+    }
+    vector<Point> Block::nextMoveBody(enum moveDirection dir){
+        vector<Point> nextBody;
+        for(const auto &point:body)
+            nextBody.push_back(point.shiftCopy(dir,0));
+
+        return nextBody;
+    }
+    void Block::rotate(){
+        for(auto &point:body)
+            nextBody.push_back(point.shiftCopy(dir,0));
+
+        return nextBody;
+    }
+    vector<Point> Block::nextRotateBody(){
+        vector<Point> nextBody;
+        for(const auto &point:body){
+            int pointX=point.getX()-pos.getX();
+            int pointY=point.getY()-pos.getY();
+            nextBody.push_back(Point((-1*pointY)+pos.getX(),pointX+pos.getY()));
+        }
+        return nextBody;
+    }
+    vector<Point>getBody(){return body;}
+    void setBody(vector<Point> body){this->body=body;}
