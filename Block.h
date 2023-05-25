99533286ae54306c2690b1869625cc8bbc3d791a
inungg <125361551+inungg@users.noreply.github.com>
2023-05-26 00:12:24 +0800

    Update Block.h

--- Block.h
+++ Block.h
@@ -1,46 +1,42 @@
 #ifndef BLOCK_H
 #define BLOCK_H
-
-#include "Cell.h"
-#include <vector>
+#include"Point.h"
+#include<vector>
 #include <stdlib.h>
-#include <time.h>
+#include<time.h>
+using namespace std;
 
-enum block_type {
-    t_block,
-    i_block,
-    o_block,
-    l_block,
-    j_block,
-    s_block,
-    z_block
+enum BlockType{
+    Block_t,
+    Block_i,
+    Block_o,
+    Block_l,
+    Block_j,
+    Block_s,
+    Block_z
 };
 
-enum move_direction {
-    right = 1,
-    left = -1
+enum moveDirection{
+    right=1,left=-1
 };
 
-class Block {
+class Block{
 private:
-    enum block_type type;
-    std::vector<Cell> body;
-    Cell pos;
-
+    enum BlockType type;
+    vector<Point> body;
+    Point pos;
 
 public:
-    Block() = default;
-    Block(Cell pos);
-    void fall_down();
-    std::vector<Cell> next_fall_down_body();
-    void move(enum move_direction dir);
-    std::vector<Cell> next_move_body(enum move_direction dir);
+    Block()=default;
+    Block(Point pos);
+    void fallDown();
+    vector<Point> nextFallDownBody();
+    void move(enum moveDirection dir);
+    vector<Point> nextMoveBody(enum moveDirection dir);
     void rotate();
-    std::vector<Cell> next_rotate_body();
-
-    std::vector<Cell> get_body() { return body; }
-    void set_body(std::vector<Cell> body) { this->body = body; }
-
+    vector<Point> nextRotateBody();
+    vector<Point>getBody(){return body;}
+    void setBody(vector<Point> body){this->body=body;}
 };
 
 #endif
