6cefa04fd7fb408ac669109ca589678e869dc63c
wennnlinggg <127189858+wennnlinggg@users.noreply.github.com>
2023-06-01 15:42:37 +0800

    Update Point.h

--- Point.h
+++ Point.h
@@ -1,24 +1,30 @@
 #ifndef POINT_H
 #define POINT_H
 
-class Point{
+#include <iostream>
+
+class Point {
 private:
     int x;
     int y;
     char type;
-
+    int color;
 public:
     Point(int x = 0, int y = 0, char type = ' ');
-    int getX()const;
-    int getY()const;
-    char getType()const;
+
+    int getX() const;
+    int getY() const;
+    char getType() const;
     void setType(char newType);
-    Point shiftCopy(int shiftX,int shiftY)const;
-    void move(int moveX,int moveY);
-    void setCoordinate(int newX,int newY);
-    bool operator==(const Point &rhs)const;
 
 
+    Point shiftCopy(int shiftX, int shiftY) const;
+    void move(int moveX, int moveY);
+    void setCoordinate(int newX, int newY);
+
+    bool operator==(const Point& rhs) const;
+    int getColor()const;
+    void setColor(int c);
 };
 
 #endif
