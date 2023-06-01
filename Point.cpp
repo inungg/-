769a4f257b462b45d41f694240655a86b8530072
wennnlinggg <127189858+wennnlinggg@users.noreply.github.com>
2023-06-01 15:42:26 +0800

    Update Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -1,33 +1,53 @@
-   #include"Point.h"
+#include "Point.h"
+using namespace std;
 
-    Point::Point(int x, int y, char type) {
-      this->x = x;
-      this->y = y;
-      this->type = type;
-   }
-    int Point::getX()const{
-        return x;
-    }
-    int Point::getY()const{
-        return y;
-    }
-    char Point::getType()const{
-        return type;
-    }
-    void Point::setType(char newType){
-        type=newType;
-    }
-    Point Point::shiftCopy(int shiftX,int shiftY)const{
-        return Point(x+shiftX,y+shiftY,type);
-    }
-    void Point::move(int moveX,int moveY){
-        x+=moveX;
-        y+=moveY;
-    }
-    void Point::setCoordinate(int newX,int newY){
-        x=newX;
-        y=newY;
-    }
-    bool Point::operator==(const Point &rhs)const{
-        return (x==rhs.getX()&&y==rhs.getY());
+
+
+Point::Point(int x, int y, char type) {
+    this->x = x;
+    this->y = y;
+    this->type = type;
+}
+
+
+int Point::getX()const {
+    return x;
+}
+
+int Point::getY()const {
+    return y;
+}
+
+char Point::getType() const {
+    return type; 
+}
+
+void Point::setType(char newType) {
+    type = newType;
+}
+
+Point Point::shiftCopy(int shiftX, int shiftY) const {
+    return Point(x + shiftX, y + shiftY, type);
+}
+
+void Point::move(int moveX, int moveY) {
+    x += moveX;
+    y += moveY;
+}
+
+void Point::setCoordinate(int newX, int newY) {
+    x = newX;
+    y = newY;
+}
+
+
+bool Point::operator==(const Point& rhs) const {
+    return (x == rhs.getX() && y == rhs.getY());
+}
+
+int Point::getColor()const {
+    return color;
+}
+void Point::setColor(int c){
+    color = c;
     }
